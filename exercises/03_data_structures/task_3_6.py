4fa71c2cb36dc4b52aa07ebe631c6d6951d4df1d
tpedko <tpedko@gmail.com>
2017-09-12 00:07:57 +0300

    add answer task 3.6

--- exercises/03_data_structures/task_3_6.py
+++ exercises/03_data_structures/task_3_6.py
@@ -15,3 +15,37 @@ Outbound Interface    FastEthernet0/0
 '''
 
 ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
+ospf_value = ospf_route.replace(',','').replace('[','').replace(']','').split()
+ospf_value[0] = 'OSPF'
+ospf_value.remove('via')
+
+print('{:24}{}'.format('Protocol:', ospf_value[0]))
+print('{:24}{}'.format('Prefix:', ospf_value[1]))
+print('{:24}{}'.format('AD/Metric:', ospf_value[2]))
+print('{:24}{}'.format('Next-Hop:', ospf_value[3]))
+print('{:24}{}'.format('Last update:', ospf_value[4]))
+print('{:24}{}'.format('Outbound Interface', ospf_value[5]))
+
+
+'''
+for t, o in :
+    print('{:25}{}'.format(t, o))
+
+ospf_list = [x for x in ospf_route.replace(',', '').split(' ') if len(x) > 1]
+
+ospf_list.insert(0, 'OSPF')
+ospf_list.remove('via')
+
+ospf_value = ['Protocol:', 'Prefix:', 'AD/Metric:', 'Next-Hop:', 'Last update:', 'Outbound Interface']
+
+ospf_value = dict.fromkeys(ospf_value)
+
+
+
+for key, value in r1.items():
+    print('{:25}{}'.format(key, value))
+
+
+for key, value in r1.items():
+    print('{:20} => {}'.format(key, (s for s in ospf_router)))
+'''
