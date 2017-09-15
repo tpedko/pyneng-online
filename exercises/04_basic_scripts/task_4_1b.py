3ebb2c22bd5cd6efe20c0e117a2876921a4ae94f
tpedko <tpedko@gmail.com>
2017-09-15 23:07:53 +0300

    add answer task 4.1b

--- exercises/04_basic_scripts/task_4_1b.py
+++ exercises/04_basic_scripts/task_4_1b.py
@@ -9,3 +9,30 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+from sys import argv
+
+ip_mask = argv[1:]
+
+ip_mask = ''.join(ip_mask[0:])
+
+
+#ip_mask = input('Введите IP-сети в формате 10.1.1.0/24: ')
+
+ip_mask = str(ip_mask)
+
+ip_mask = ip_mask.split('/')
+ip = ip_mask[0]
+mask = ip_mask[1]
+
+ip_bit = ''.join('{:>08b}'.format(int(x)) for x in ip.split('.'))
+ip_bit = ip_bit[0:int(mask)]+'0'*(32-int(mask))
+
+print('Network:')
+print('{:<10}{:<10}{:<10}{:<10}'.format(int(ip_bit[0:8],2),int(ip_bit[8:16],2),int(ip_bit[16:24],2),int(ip_bit[24:32],2)))
+print('{:<10}{:<10}{:<10}{:<10}'.format(ip_bit[0:8],ip_bit[8:16],ip_bit[16:24],ip_bit[24:32]))
+
+print('\nMask:')
+print('/{}'.format(mask))
+mask = '{:<b}{}'.format(2**int(mask)-1,'0'*(32-int(mask)))
+print('{:<10}{:<10}{:<10}{:<10}'.format(int(mask[0:8],2),int(mask[8:16],2),int(mask[16:24],2),int(mask[24:32],2)))
+print('{:<10}{:<10}{:<10}{:<10}'.format(mask[0:8],mask[8:16],mask[16:24],mask[24:32]))
