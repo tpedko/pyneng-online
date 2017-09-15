8d0948bb77c39fb990480beb4c4d245d15565761
tpedko <tpedko@gmail.com>
2017-09-15 17:39:38 +0300

    add answer task 3.9

--- exercises/04_basic_scripts/task_4_1a.py
+++ exercises/04_basic_scripts/task_4_1a.py
@@ -31,3 +31,21 @@ Mask:
 
 '''
 
+ip_mask = input('Введите IP-сети в формате 10.1.1.0/24: ')
+
+ip_mask = ip_mask.split('/')
+ip = ip_mask[0]
+mask = ip_mask[1]
+
+print('Network:')
+print(''.join('{:<10}'.format(x) for x in ip.split('.')))
+print('  '.join('{:>08b}'.format(int(x)) for x in ip.split('.')))
+print('\nMask:')
+#print('/', mask, sep='')
+print('/{}'.format(mask))
+'''
+mask = ('1' * int('mask')+('0' * 32))[:32]
+'''
+mask = '{:<b}{}'.format(2**int(mask)-1,'0'*(32-int(mask)))
+print('{:<10}{:<10}{:<10}{:<10}'.format(int(mask[0:8],2),int(mask[8:16],2),int(mask[16:24],2),int(mask[24:32],2)))
+print('{:<10}{:<10}{:<10}{:<10}'.format(mask[0:8],mask[8:16],mask[16:24],mask[24:32]))
