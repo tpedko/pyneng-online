21b8af2383cf35444645400044ed2e9c2fbdbe52
tpedko <tpedko@gmail.com>
2017-09-17 00:44:18 +0300

    add answer task 3.9

--- exercises/03_data_structures/task_3_9.py
+++ exercises/03_data_structures/task_3_9.py
@@ -18,6 +18,7 @@
 num_list = [10, 2, 30, 100, 10, 50, 11, 30, 15, 7]
 word_list = ['python', 'ruby', 'perl', 'ruby', 'perl', 'python', 'ruby', 'perl']
 
+
 num_list.reverse()
 print(len(num_list) - num_list.index(10)-1)
 
