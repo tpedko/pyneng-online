ece4416517dc02ccb00d6be4bd386e34934b0bc3
tpedko <tpedko@gmail.com>
2017-09-17 22:28:55 +0300

    add answer task 4.2d

--- exercises/04_basic_scripts/task_4_2d.py
+++ exercises/04_basic_scripts/task_4_2d.py
@@ -43,3 +43,8 @@ london_co = {
     'routing': True
     }
 }
+
+device = input('Enter device name r1, r2,sw1: ')
+param = input('Enter parameter name ('+', '.join((london_co[device].keys()))+'): ')
+param = param.lower()
+print(london_co.get(device).get(param, 'Такого параметра нет'))
