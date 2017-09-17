1b006387507e8095a9916b5f6ddb00180944ca88
tpedko <tpedko@gmail.com>
2017-09-17 22:06:30 +0300

    add answer task 4.2b

--- exercises/04_basic_scripts/task_4_2b.py
+++ exercises/04_basic_scripts/task_4_2b.py
@@ -44,3 +44,7 @@ london_co = {
     'routing': True
     }
 }
+
+device = input('Enter device name r1, r2,sw1: ')
+param = input('Enter parameter name ('+', '.join((london_co['sw1'].keys()))+'): ')
+print(london_co[device][param])
