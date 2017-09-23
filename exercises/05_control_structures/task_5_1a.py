9be9c1d90a1d3168de4f21cb4765bccfd6181f1b
tpedko <tpedko@gmail.com>
2017-09-23 23:49:02 +0300

    add task 5.1a

--- exercises/05_control_structures/task_5_1a.py
+++ exercises/05_control_structures/task_5_1a.py
@@ -16,3 +16,28 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+
+ip_addr = input('Введите IP-сети в формате 10.1.1.1: ')
+
+ip_addr = ip_addr.split('.')
+
+if (len(ip_addr) != 4):
+    print('Incorrect IPv4 address')
+elif (int(ip_addr[0]) < 0 or int(ip_addr[0]) >255):
+    print('Incorrect IPv4 address')
+elif (int(ip_addr[1]) < 0 or int(ip_addr[1]) >255):
+    print('Incorrect IPv4 address')
+elif (int(ip_addr[2]) < 0 or int(ip_addr[2]) >255):
+    print('Incorrect IPv4 address')
+elif (int(ip_addr[3]) < 0 or int(ip_addr[3]) >255):
+    print('Incorrect IPv4 address')
+elif (int(ip_addr[0]) >= 1 and int(ip_addr[0]) <= 223):
+    print('unicast')
+elif (int(ip_addr[0]) >= 224 and int(ip_addr[0]) <= 239):
+    print('multicast')
+elif (int(ip_addr[0]) == 255 and int(ip_addr[1]) == 255 and int(ip_addr[2]) == 255 and int(ip_addr[3]) == 255):
+    print('local broadcast')
+elif (int(ip_addr[0]) == 0 and int(ip_addr[1]) == 0 and int(ip_addr[2]) == 0 and int(ip_addr[3]) == 0):
+    print('unassigned')
+else:
+    print('unused')
