d55ead3742aaba0d3576d49505de7f80193c65bc
tpedko <tpedko@gmail.com>
2017-09-23 22:48:58 +0300

    add task 5.1

--- exercises/05_control_structures/task_5_1.py
+++ exercises/05_control_structures/task_5_1.py
@@ -21,3 +21,16 @@ D: 224-239
 '''
 
 ip_addr = input('Введите IP-сети в формате 10.1.1.1: ')
+
+ip_addr = ip_addr.split('.')
+
+if (int(ip_addr[0]) >= 1 and int(ip_addr[0]) <= 223):
+    print('unicast')
+elif (int(ip_addr[0]) >= 224 and int(ip_addr[0]) <= 239):
+    print('multicast')
+elif (int(ip_addr[0]) == 255 and int(ip_addr[1]) == 255 and int(ip_addr[2]) == 255 and int(ip_addr[3]) == 255):
+    print('local broadcast')
+elif (int(ip_addr[0]) == 0 and int(ip_addr[1]) == 0 and int(ip_addr[2]) == 0 and int(ip_addr[3]) == 0):
+    print('unassigned')
+else:
+    print('unused')
